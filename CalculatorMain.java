a11706115b6dcd15d0e9d296ef7bddc52cb8488b
unknown <neha.nazre@wissen.com>
2025-07-03 16:21:03 +0530

    Second commit

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -2,6 +2,8 @@ public class CalculatorMain {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         int result = calc.add(10, 20);
+	int subRes = calc.subtract(20,20);
         System.out.println("Result: " + result);
+	System.out.println("Result subtraction: " + subRes);
     }
 }
